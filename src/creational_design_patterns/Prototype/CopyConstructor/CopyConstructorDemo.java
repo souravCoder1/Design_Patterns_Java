e00705c544d778d71a26610ec52314c32ac461bf
souravpal01 <souravpal7217@gmail.com>
2023-10-01 15:27:51 +0530

    Prototype design pattern - CopyConstructorDemo

---
+++ src/creational_design_patterns/Prototype/CopyConstructor/CopyConstructorDemo.java
@@ -0,0 +1,17 @@
+package creational_design_patterns.Prototype.CopyConstructor;
+
+class CopyConstructorDemo
+{
+  public static void main(String[] args)
+  {
+    Employee john = new Employee("John",
+      new Address("123 London Road", "London", "UK"));
+
+    //Employee chris = john;
+    Employee chris = new Employee(john);
+
+    chris.name = "Chris";
+    System.out.println(john);
+    System.out.println(chris);
+  }
+}
